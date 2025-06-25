17d4e7c98c22636040f684b0817c2ce2586e7543
horsi2410 <guilherme241098@gmail.com>
2025-06-25 12:32:33 -0400

    sim

---
+++ src/routes/07/irmandades/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    let {form} =$props();
+</script>
+<form method="post">
+    nome: <input name='nome' type='text' placeholder="Nome do aprendiz"/> <br> 
+    nascimento: <input name='nascimento' type='date'/> <br> 
+    força: <input name='forca' type='number' min = '1' max='10'/> <br> 
+    inteligencia: <input name='inteligencia' type='number'/> <br> 
+    destreza: <input name='destreza' type='number' min='1' max='10'/> <br> 
+    conhecimento de magias: <input name='magia' type='checkbox'/> <br> 
+    ferramentas de artesão: <input name='ferramentas' type='checkbox'/> <br> 
+    <button formaction='?/guerreiros'>Inscrever na Ordem dos guerreiros</button><br>
+    <button formaction='?/magos'>Inscrever no Circulo dos Magos</button><br>
+    <button formaction='?/artesao'>Inscrever na Guilda dos Artesão</button><br>
+</form>
+{#if form?.eror}
+<p style='color:red'> {form.error}</p>
+{/if}
